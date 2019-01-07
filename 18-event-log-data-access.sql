0b76e284d7f39b2cf5c6d18dd5fd4a8a09b94b22
leondutoit <dutoit.leon@gmail.com>
2019-01-07 11:23:02 +0100

    clear out event logs after demo

--- 18-event-log-data-access.sql
+++ 18-event-log-data-access.sql
@@ -5,5 +5,6 @@ set session "request.jwt.claim.user" = '';
 select * from event_log_data_access;
 
 -- who looked at A's data, when
-select request_time, data_user from event_log_data_access
+select data_owner, data_user, row_id, request_time
+    from event_log_data_access
     where data_owner = 'owner_A';
--- run-demo.sh
+++ run-demo.sh
@@ -2,12 +2,15 @@
 
 num_steps=$( ls | grep sql | wc -l )
 
-psql -d tsd_backend_utv_auth -c 'drop table personal_details; drop table spending_habits; drop role group1; drop role group2;'
+psql -d tsd_backend_utv_auth -c 'drop table if exists personal_details; drop table if exists spending_habits; drop role group1; drop role group2;'
 
 for i in `seq 1 $num_steps`; do
     step=$( ls $i-* )
-    echo $step
+    clear
+    echo "Press any key to execute: $step"
+    read
     psql -d tsd_backend_utv_auth -e -f $step
+    read
 done
 
-psql -d tsd_backend_utv_auth -c 'delete from event_log_access_control; delete from event_log_data_access;'
+psql -d tsd_backend_utv_auth -c 'delete from event_log_access_control; delete from event_log_data_access; delete from event_log_user_data_deletions; delete from event_log_user_group_removals; delete from event_log_data_updates;'
